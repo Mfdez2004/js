fef44d6e5ad6338bfd1ef91213c97e5da10d98e4
Mfdez2004 <miguelafernandez2004@gmail.com>
2024-11-18 20:23:18 +0100

    Add files via upload

--- particulas.js
+++ particulas.js
@@ -1,1 +1,105 @@
-function _0x4edf(_0x31931f,_0x1dac0c){var _0x430a32=_0x430a();return _0x4edf=function(_0x4edf6b,_0x48963f){_0x4edf6b=_0x4edf6b-0x135;var _0x5ba374=_0x430a32[_0x4edf6b];return _0x5ba374;},_0x4edf(_0x31931f,_0x1dac0c);}function _0x430a(){var _0x5a0ee3=['top','bold','black','2556829JwUTtU','Computer\x20Modern','5269avDIsz','width','oblique','#cargaEl','110ufSUPx','1465QbwrXN','Rect','toString','red','Text','results-c2','getElementById','left','add','height','2133551fgzRSj','abs','1560753MvNIHC','9aLSYvr','2711432dOpPMC','#simbolo','right','367976PkkbrR','blue','center','Canvas','278JqtrLS','20904ZrMChO','purple'];_0x430a=function(){return _0x5a0ee3;};return _0x430a();}(function(_0x5c8e96,_0x4cda3c){var _0x4ed55a=_0x4edf,_0xe175bd=_0x5c8e96();while(!![]){try{var _0x5ebddc=-parseInt(_0x4ed55a(0x154))/0x1*(parseInt(_0x4ed55a(0x14c))/0x2)+-parseInt(_0x4ed55a(0x143))/0x3+parseInt(_0x4ed55a(0x145))/0x4+parseInt(_0x4ed55a(0x137))/0x5*(-parseInt(_0x4ed55a(0x14d))/0x6)+-parseInt(_0x4ed55a(0x141))/0x7+-parseInt(_0x4ed55a(0x148))/0x8*(parseInt(_0x4ed55a(0x144))/0x9)+-parseInt(_0x4ed55a(0x136))/0xa*(-parseInt(_0x4ed55a(0x152))/0xb);if(_0x5ebddc===_0x4cda3c)break;else _0xe175bd['push'](_0xe175bd['shift']());}catch(_0x246641){_0xe175bd['push'](_0xe175bd['shift']());}}}(_0x430a,0x9508b),(function(){var _0x42466b=_0x4edf,_0x189b68=new fabric[(_0x42466b(0x14b))]('c2');results1=document[_0x42466b(0x13d)](_0x42466b(0x13c));var _0x39cb26=eval(new String(_0x42466b(0x146))),_0x32483e=eval('#Z'),_0x3b9ed0=eval('#A'),_0x1abd47=eval(_0x42466b(0x135)),_0xa80155='';switch(_0x1abd47){case 0x0:_0xa80155='';break;case 0x1:_0xa80155='+';break;case-0x1:_0xa80155='-';break;default:cargaAbs=Math[_0x42466b(0x142)](_0x1abd47),_0xa80155=cargaAbs['toString']();_0x1abd47<0x0&&(_0xa80155=_0xa80155+'-');;_0x1abd47>0x0&&(_0xa80155=_0xa80155+'+');break;}var _0x31343e=_0x189b68[_0x42466b(0x140)],_0x146786=_0x189b68[_0x42466b(0x155)],_0x594dd3=0x3;const _0x21a893=0x19,_0x249428=0x64,_0x26eb6b=new fabric['Text'](_0x39cb26,{'originX':_0x42466b(0x14a),'originY':_0x42466b(0x14a),'left':0.5*_0x146786,'top':0.5*_0x31343e,'stroke':_0x42466b(0x151),'strokeUniform':!![],'fontFamily':_0x42466b(0x153),'fontStyle':'oblique','fontWeight':_0x42466b(0x150),'fontSize':0x5a,'selectable':![]});var _0x21dbd7=_0x26eb6b[_0x42466b(0x140)],_0x51812f=_0x26eb6b[_0x42466b(0x155)],_0xddf84f=new fabric[(_0x42466b(0x138))]({'top':0x0,'left':0x0,'width':_0x146786-_0x594dd3,'height':_0x31343e-_0x594dd3,'fill':'PeachPuff','stroke':'#993300','strokeWidth':_0x594dd3,'selectable':![]});const _0x483b6c=new fabric[(_0x42466b(0x13b))](_0x3b9ed0[_0x42466b(0x139)](),{'originX':_0x42466b(0x147),'originY':_0x42466b(0x14a),'left':_0x26eb6b[_0x42466b(0x13e)]-_0x26eb6b['width']/0x2,'top':_0x26eb6b['top']-0x28,'stroke':_0x42466b(0x13a),'strokeUniform':![],'fontFamily':_0x42466b(0x153),'fontStyle':_0x42466b(0x156),'fontWeight':'bold','fontSize':0x1e,'selectable':![]}),_0x166bb0=new fabric[(_0x42466b(0x13b))](_0x32483e[_0x42466b(0x139)](),{'originX':'right','originY':'center','left':_0x26eb6b[_0x42466b(0x13e)]-_0x26eb6b[_0x42466b(0x155)]/0x2-0x4,'top':0x82,'stroke':_0x42466b(0x149),'strokeUniform':![],'fontFamily':_0x42466b(0x153),'fontStyle':_0x42466b(0x156),'fontWeight':_0x42466b(0x150),'fontSize':0x1e,'selectable':![]}),_0x4bdae9=new fabric['Text'](_0xa80155,{'originX':_0x42466b(0x13e),'originY':_0x42466b(0x14a),'left':_0x26eb6b['left']+_0x26eb6b['width']/0x2+0xa,'top':_0x26eb6b[_0x42466b(0x14f)]-0x28,'stroke':_0x42466b(0x14e),'strokeUniform':![],'fontFamily':_0x42466b(0x153),'fontStyle':'oblique','fontWeight':_0x42466b(0x150),'fontSize':0x1e,'selectable':![]});_0x189b68[_0x42466b(0x13f)](_0xddf84f,_0x26eb6b,_0x483b6c,_0x166bb0,_0x4bdae9);}()));
+(function() {
+        // Initiate a Canvas instance  
+        var canvas1  = new fabric.Canvas('c1');
+	results1 = document.getElementById('results-c1');
+        var simbolo = '#simbolo';
+        var Z = eval('#Z');
+        var A = eval('#A');
+        var carga = eval('#cargaEl');
+        var cargaTxt = "";
+        switch (carga) {
+            case 0:
+                cargaTxt = "";
+                break;
+            case 1:
+                cargaTxt = "+";
+                break;
+            case -1:
+                cargaTxt = "-";
+                break;
+            default:
+                cargaAbs = Math.abs(carga);
+                cargaTxt = cargaAbs.toString();
+                if (carga < 0) {
+                    cargaTxt = cargaTxt + "-"
+                };
+                if (carga > 0) {
+                    cargaTxt = cargaTxt + "+"
+                }
+                break;
+        }
+
+        var altura = canvas1.height;
+        var anchura = canvas1.width;
+        var anchoLinea = 3;
+        // Initiate a Rect instance
+        const orX = 25;
+        const orY = 100;
+        const txtSymbol = new fabric.Text(simbolo, {
+            originX: 'center',
+            originY: 'center',
+            left: 0.5 * anchura,
+            top: 0.5 * altura,
+            stroke: 'black',
+            strokeUniform: true,
+            fontFamily: 'Computer Modern',
+            fontStyle: 'oblique',
+            fontWeight: 'bold',
+            fontSize: 90,
+            "selectable": false
+        });
+        var symbHeight = txtSymbol.height;
+        var symbWidth = txtSymbol.width;
+
+        var rec = new fabric.Rect({
+            top: 0,
+            left: 0,
+            width: anchura - anchoLinea,
+            height: altura - anchoLinea,
+            fill: 'PeachPuff',
+            stroke: '#993300',
+            strokeWidth: anchoLinea,
+            "selectable": false
+        });
+        const txtA = new fabric.Text(A.toString(), {
+            originX: 'right',
+            originY: 'center',
+            left: txtSymbol.left - txtSymbol.width / 2,
+            top: txtSymbol.top - 40,
+            stroke: 'red',
+            strokeUniform: false,
+            fontFamily: 'Computer Modern',
+            fontStyle: 'oblique',
+            fontWeight: 'bold',
+            fontSize: 30,
+            "selectable": false
+        });
+        const txtZ = new fabric.Text(Z.toString(), {
+            originX: 'right',
+            originY: 'center',
+            left: txtSymbol.left - txtSymbol.width / 2 - 4,
+            top: 130,
+            stroke: 'blue',
+            strokeUniform: false,
+            fontFamily: 'Computer Modern',
+            fontStyle: 'oblique',
+            fontWeight: 'bold',
+            fontSize: 30,
+            "selectable": false
+        });
+        const txtCarga = new fabric.Text(cargaTxt, {
+            originX: 'left',
+            originY: 'center',
+            left: txtSymbol.left + txtSymbol.width / 2 + 10,
+            top: txtSymbol.top - 40,
+            stroke: 'purple',
+            strokeUniform: false,
+            fontFamily: 'Computer Modern',
+            fontStyle: 'oblique',
+            fontWeight: 'bold',
+            fontSize: 30,
+            "selectable": false
+        });
+        canvas1.add(rec, txtSymbol, txtA, txtZ, txtCarga);
+	//this.__canvases.push(canvas1);
+})();
